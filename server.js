373b157559a8ebf3f07e56b510a48bf5e2efe327
kmuzzi10 <kmuzammil12546@gmail.com>
2023-11-22 04:01:44 +0500

    working

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js"
+import authRoutes from "./routes/authRoute.js";
+
+dotenv.config();
+
+
+//database config
+connectDB();
 
 const app = express();
 
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+//routes
+
+app.use("/api/v1/auth", authRoutes);
+
+
+
+//rest API
 app.get('/',(req,res)=>{
 res.send('<h1>Welcome</h1>')
 });
@@ -16,7 +38,7 @@ res.send('<h1>Welcome</h1>')
 
 
 
-const port = 8080;
+const port = process.env.PORT;
 
 app.listen(port,()=>{
     console.log(`server is running on ${port}`);
