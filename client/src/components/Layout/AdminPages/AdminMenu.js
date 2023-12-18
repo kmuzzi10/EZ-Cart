73761b6e0f891cfed1ad13c618667d9d1b403a6a
kmuzzi10 <kmuzammil12546@gmail.com>
2023-12-19 00:03:12 +0500

    Working

--- client/src/components/Layout/AdminPages/AdminMenu.js
+++ client/src/components/Layout/AdminPages/AdminMenu.js
@@ -9,7 +9,9 @@ const AdminMenu = () => {
                 <div className="list-group">
                     <NavLink to='/dashboard/admin/create-category' className="list-group-item list-group-item-action">Create Category</NavLink>
                     <NavLink to='/dashboard/admin/create-product' className="list-group-item list-group-item-action">Create Products</NavLink>
+                    <NavLink to='/dashboard/admin/products' className="list-group-item list-group-item-action">Products</NavLink>
                     <NavLink to='/dashboard/admin/users' className="list-group-item list-group-item-action">Users</NavLink>
+                    
 
                 </div>
             </div>
--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import AdminMenu from '../../components/Layout/AdminPages/AdminMenu'
+import Layout from '../../components/Layout/Layout'
+import axios from "axios"
+import { Link } from "react-router-dom"
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
+
+    const [products, setProducts] = useState([])
+
+    //get all products
+    const getAllProducts = async () => {
+        try {
+            const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/get-product`)
+            setProducts(data.products)
+        } catch (error) {
+            console.log(error)
+            alert('Something Went Wrong')
+        }
+    }
+
+    //life cycle
+    useEffect(() => {
+        getAllProducts()
+    }, [])
+
+    return (
+        <Layout>
+            <div className='fluid-container m-3 p-3'>
+                <div className='row'>
+                    <div className='col-lg-3 col-md-3 col-sm-4'>
+                        <AdminMenu />
+                    </div>
+                    <div className='col-lg-9 col-md-9 col-sm-8'>
+                        <h1 className='text-center'>All Products List</h1>
+                        <div className='d-flex'>
+                            {products?.map(p => (
+                                <Link to={`/dashboard/admin/product/${p.slug}`} key={p._id} className='product-link'>
+                                    <div className="card m-3" style={{ width: '18rem' }}>
+                                        <img src={p.photo} className="card-img-top" alt={p.name} />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{p.name}</h5>
+                                            <p className="card-text">{p.description}.</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </Layout>
+    )
 }
 
 export default Products
